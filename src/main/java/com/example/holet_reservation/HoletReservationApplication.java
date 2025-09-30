b1f1d560402ea45fb71548e005e248e4e09df685
Nikolas Sakovich <koksak2013@yandex.ru>
2025-09-30 21:29:10 +0300

    Initial commit

---
+++ src/main/java/com/example/holet_reservation/HoletReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.holet_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoletReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoletReservationApplication.class, args);
+	}
+
+}
